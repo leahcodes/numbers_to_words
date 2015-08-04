167a7f85f05656381a3fb9de9e7aadf0e917a9bf
Evan Clough & Leah Klamkin <student@epicodus.com>
2015-08-04 16:43:27 -0700

    Handles single-digit numbers and teens

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,15 +1,29 @@
+#     # zero_to_nineteen = { 0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen" }
+#     #
+#     multiple_tens = { 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety" }
+#
+#   word_array = []
+#   number_array = self.to_s.split("")
+#     if number_array.length == 1
+#       puts zero_to_nine.fetch(self)
+#     end
+#   end
+# end
+
 class Fixnum
   define_method(:numbers_to_words) do
 
-    # zero_to_nineteen = { 0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen" }
-    #
-    # multiple_tens = { 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety" }
+    single_digit = { 0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
 
-  zero_to_nine = { 0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
+    teens = { 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen" }
 
-  number_array = self.to_s.split("")
-    if number_array.length == 1
-      zero_to_nine.fetch(self)
+    number_array = self.to_s.split("")
+
+    if teens.include?(self)
+      teens.fetch(self)
+    elsif number_array.length == 1
+      single_digit.fetch(self)
     end
+    
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -2,7 +2,11 @@ require('rspec')
 require('numbers_to_words')
 
 describe('Fixnum#numbers_to_words') do
-  it("returns the name of the number in English for a given Fixnum between 0 and 9") do
+  it("handles single-digit numbers") do
     expect(8.numbers_to_words()).to(eq("eight"))
+  end
+
+  it("handles teens") do
+    expect(13.numbers_to_words()).to(eq("thirteen"))
   end
 end
