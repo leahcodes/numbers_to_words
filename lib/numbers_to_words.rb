e51862829297b315f2ce496e0fd88504edab4fa5
Evan Clough & Leah Klamkin <student@epicodus.com>
2015-08-04 15:47:23 -0700

    Returns the name of the number in English for a given Fixnum between 0 and 9.

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,9 +1,15 @@
 class Fixnum
   define_method(:numbers_to_words) do
 
-    zero_to_nineteen = { 0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen" }
+    # zero_to_nineteen = { 0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen" }
+    #
+    # multiple_tens = { 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety" }
 
-    zero_to_nineteen.fetch(self)
+  zero_to_nine = { 0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
 
+  number_array = self.to_s.split("")
+    if number_array.length == 1
+      zero_to_nine.fetch(self)
+    end
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -2,7 +2,7 @@ require('rspec')
 require('numbers_to_words')
 
 describe('Fixnum#numbers_to_words') do
-  it('returns the name of the number in English for a given Fixnum between 0 and 19') do
-    expect(12.numbers_to_words).to(eq("twelve"))
+  it("returns the name of the number in English for a given Fixnum between 0 and 9") do
+    expect(8.numbers_to_words()).to(eq("eight"))
   end
 end
